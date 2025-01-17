ddf849406ff2de0b5b50043da3a3ee61fce85c9d
Your Name <ekka.beaula@gmail.com>
2025-01-17 12:01:59 +0100

    initialize ejs

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+//config ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(8000, () => console.log("server started"));
