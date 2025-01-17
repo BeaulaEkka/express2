667bb35bef77f66228fd427010c4505fb2d4cd4b
Your Name <ekka.beaula@gmail.com>
2025-01-17 14:22:42 +0100

    welcome page

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Home Page",
+    message: "Welcome page",
+  });
 });
 
 app.listen(8000, () => console.log("server started"));
